4c1bc08c69021ef3c312993b312671a842ae0f88
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-03-29 21:22:20 +0000

    feat: GeneroModel and GenderoRepository

--- src/main/java/application/model/Genero.java
+++ src/main/java/application/model/Genero.java
@@ -2,10 +2,31 @@ package application.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "Genero")
 public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
