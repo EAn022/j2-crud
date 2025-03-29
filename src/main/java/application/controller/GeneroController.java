5f70a60cd9c8a00920f8ed6f367efaafde3355b9
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-03-29 22:17:08 +0000

    wip: GeneroController.select( )

--- src/main/java/application/controller/GeneroController.java
+++ src/main/java/application/controller/GeneroController.java
@@ -1,5 +1,23 @@
 package application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import application.repository.GeneroRepository;
+
+@Controller
+@RequestMapping(value = "/genero")
 public class GeneroController {
     
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @RequestMapping(value = "/list")
+    public String select(Model ui){
+        ui.addAttribute("generos", "100");
+        
+        return "list";
+    }
 }
