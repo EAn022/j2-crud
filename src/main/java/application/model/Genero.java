a823ef53b5b81991857ca38fddc1b88b23a9015a
EAn022 <162371161+EAn022@users.noreply.github.com>
2025-04-25 17:33:40 +0000

    feat: model livro.java

--- src/main/java/application/model/Genero.java
+++ src/main/java/application/model/Genero.java
@@ -1,5 +1,6 @@
 package application.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ public class Genero {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String nome;
 
     public long getId() {
---
+++ src/main/java/application/model/Livro.java
@@ -0,0 +1,23 @@
+package application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genero", nullable = false)
+    private Genero genero;
+    
+}
